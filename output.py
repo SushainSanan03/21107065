2deffd2b294527f818ea327b3822c5e8d5de41f8
SushainSanan03 <104348775+SushainSanan03@users.noreply.github.com>
2022-04-25 01:19:23 -0700

    Add files via upload

---
+++ output.py
@@ -0,0 +1,44 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============================================ RESTART: C:\Users\DELL\AppData\Local\Programs\Python\Python310\21107065.py ===========================================
+QUESTION 1
+Enter the First Number:80
+Enter the Second Number:69
+Enter the Third Number:65
+The Average of these three numbers is : 71.33333333333333
+QUESTION 2
+Enter the Gross Income :1450000
+Enter the Number of Dependents :4
+Your Tax is : 285600.0
+QUESTION 3
+Enter seconds: 3636
+60 Minutes and 36 Seconds
+QUESTION 4
+75
+<class 'str'>
+QUESTION 5
+0 --- 0.0 1.0
+15 --- 0.2588 0.9659
+30 --- 0.5 0.866
+45 --- 0.7071 0.7071
+60 --- 0.866 0.5
+75 --- 0.9659 0.2588
+90 --- 1.0 0.0
+105 --- 0.9659 -0.2588
+120 --- 0.866 -0.5
+135 --- 0.7071 -0.7071
+150 --- 0.5 -0.866
+165 --- 0.2588 -0.9659
+180 --- 0.0 -1.0
+195 --- -0.2588 -0.9659
+210 --- -0.5 -0.866
+225 --- -0.7071 -0.7071
+240 --- -0.866 -0.5
+255 --- -0.9659 -0.2588
+270 --- -1.0 -0.0
+285 --- -0.9659 0.2588
+300 --- -0.866 0.5
+315 --- -0.7071 0.7071
+330 --- -0.5 0.866
+345 --- -0.2588 0.9659
