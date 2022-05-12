370e939b10d7c0cc32c56a4b37510213f422f105
SushainSanan03 <104348775+SushainSanan03@users.noreply.github.com>
2022-05-12 10:38:18 -0700

    Add files via upload

---
+++ OUTPUT 3.py
@@ -0,0 +1,55 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\DELL\Downloads\assignment 3.py ===============
+QUESTION 1
+Enter a number45
+Binary equivalent of 45 is 101101
+QUESTION 2
+Enter expression:56
+56 = 56
+QUESTION 3
+Enter an integer:42
+x) 3740
+y) 861.0
+enter radius:17
+c) 3631.6811075498013
+Enter angle 1:11
+Enter angle 1:14
+d) 4.084419522932348
+Enter x coordinate of first point:24
+Enter y coordinate of first point:31
+Enter x coordinate of second point:17
+Enter y coordinate of second point:11
+M) 2.857142857142857
+QUESTION 4
+4.(a)
+0
+1
+2
+3
+4
+4.(b)
+3
+4
+5
+6
+7
+8
+9
+4.(c)
+4
+7
+10
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+QUESTION 5
+Enter number of carbon atoms in molecule:6
+Enter number of hydrogen atoms in molecule:1
+Enter number of oxygen atoms in molecule:8
+The molecular weight of the compound is 201.06734
