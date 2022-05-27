ff274916d506b4091f304ae5aeb8d9ed274136b4
SushainSanan03 <104348775+SushainSanan03@users.noreply.github.com>
2022-05-27 22:04:46 +0530

    Add files via upload

---
+++ output 4.py
@@ -0,0 +1,34 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\DELL\Downloads\assignment 4.py ===============
+Q1
+Enter mark:99
+Grade: A
+Continue?(y/n)n
+Q2
+Enter year:2002
+Not a leap year
+Q3
+Question 1:2x1=2
+Right!
+Question 2:0x0=0
+Right!
+Question 3:2x2=4
+Right!
+Question 4:1x4=4
+Right!
+Question 5:0x7=0
+Right!
+Question 6:8x3=24
+Right!
+Question 7:4x6=25
+Wrong. The correct answer is 24
+Question 8:5x5=25
+Right!
+Question 9:1x2=2
+Right!
+Question 10:7x6=42
+Right!
+Q4
+Answer: 177
