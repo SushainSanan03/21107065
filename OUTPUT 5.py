6f210bae3b3e6d4277bf1738761f64d377edab64
SushainSanan03 <104348775+SushainSanan03@users.noreply.github.com>
2022-06-15 23:30:07 +0530

    Add files via upload

---
+++ OUTPUT 5.py
@@ -0,0 +1,80 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:\Users\DELL\Downloads\assignment 5.py ===============
+Q1
+Enter a string(without apostrophes) and it will be reversed:MECHANICAL
+Reversed string: LACINAHCEM
+
+Q2
+Enter starting range:3
+Enter ending range:5
+Enter a number for which the entered range will be divisible by:8
+
+Q3
+Enter length of first side of the triangle:12
+Enter length of second side of the triangle:24
+Enter length of third side of the triangle:364
+Not a valid triangle!
+
+Q4
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * 
+* * * 
+* * 
+* 
+
+Q5
+Enter number of rows:3
+A 
+B C 
+D E F 
+
+Q6
+Prime numbers in an input range.
+Enter starting of range(>=2):9
+Enter ending of range:3
+
+Q7
+77
+154
+231
+308
+385
+462
+
+Q8
+Enter integer:52
+Enter integer:42
+Enter integer:12
+Enter integer:62
+Enter integer:32
+Enter integer:72
+Enter integer:82
+Enter integer:92
+Enter integer:22
+a) Positive integers: [52, 42, 12, 62, 32, 72, 82, 92, 22]
+b) Negative integers: []
+c) Odd integers: []
+d) Even integers: [52, 42, 12, 62, 32, 72, 82, 92, 22]
+e)
+integer:52 ; number of times it occurs: 1
+integer:42 ; number of times it occurs: 1
+integer:12 ; number of times it occurs: 1
+integer:62 ; number of times it occurs: 1
+integer:32 ; number of times it occurs: 1
+integer:72 ; number of times it occurs: 1
+integer:82 ; number of times it occurs: 1
+integer:92 ; number of times it occurs: 1
+integer:22 ; number of times it occurs: 1
+
+Q9
+Enter a list of words(strings):['Hello','how','are','you?']
+word:Hello ; number of times it occurs:1
+word:how ; number of times it occurs:1
+word:are ; number of times it occurs:1
+word:you? ; number of times it occurs:1
