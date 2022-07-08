c45a8148d0cdef908353925a955d6c2fe241fc4e
SushainSanan03 <104348775+SushainSanan03@users.noreply.github.com>
2022-07-08 22:56:21 +0530

    Add files via upload

---
+++ output 6.py
@@ -0,0 +1,44 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/DELL/Downloads/assignment 6.py ===============
+Q1
+True
+Q2
+Enter a word, phrase or sequence to see if it is a palindrome:mechanical
+It is not a palindrome.
+Q3
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+Q4
+Enter a string to check if it is a pangram: my sid ia 21107065
+It is not a pangram.
+Q5
+Enter a hyphen-separated sequence of words: hello-this-is-btechgeeks
+btechgeeks-hello-is-this
+Q6
+
+Student ID: 21107221
+Student Name: Matthew
+
+Student ID: 21101201
+Student Name: Henry
+
+Student Name: Henry
+Student Class: 12
+Q7
+<__main__.Student object at 0x0000020A9F3D2B30>
+Navodit
+<__main__.Marks object at 0x0000020A9F3D2AD0>
+70
+82
+class Student: <class 'type'> class Marks: <class 'type'>
+Q8
+[[-10, 2, 8], [-7, -3, 10]]
+Q9
+True
+False
+True
